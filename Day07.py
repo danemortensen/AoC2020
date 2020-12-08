bba94f185c0c811d545e79801cd64470e8c1340d
Dane Mortensen <mortensendane@gmail.com>
2020-12-08 01:24:21 -0800

    Day 7 solution part 2

--- Day07.py
+++ Day07.py
@@ -6,22 +6,24 @@ def getRules(filename):
         Util.regexFileHandler(r'^([a-z\ ]+)\ bags\ contain\ (.+)\.$'))
     return {p[0]: p[1] for p in parsed}
 
-def getColorAdjacencies(rules):
-    colorPattern = re.compile(r'^\d+\ ([a-z]+\ [a-z]+)\ bags?$')
-    adjacencies = {}
+def getAdjacencies(rules):
+    colorPattern = re.compile(r'^(\d+)\ ([a-z]+\ [a-z]+)\ bags?$')
+    forward, backward = {}, {}
     for color, rule in rules.items():
-        if color not in adjacencies:
-            adjacencies[color] = set()
+        forward[color] = []
+        if color not in backward:
+            backward[color] = set()
         if rule == 'no other bags':
             continue
         for contents in rule.split(', '):
             match = colorPattern.match(contents)
             assert match, f'Regex did not match str: "{contents}"'
-            colorContained = match.groups()[0]
-            if colorContained not in adjacencies:
-                adjacencies[colorContained] = set()
-            adjacencies[colorContained].add(color)
-    return adjacencies
+            numContained, colorContained = match.groups()
+            forward[color].append((colorContained, numContained))
+            if colorContained not in backward:
+                backward[colorContained] = set()
+            backward[colorContained].add(color)
+    return forward, backward
 
 def traverseAdjacencies(adjacencies, start):
     visited = set([start])
@@ -34,17 +36,33 @@ def traverseAdjacencies(adjacencies, start):
     _traverseAdjacencies(start)
     return visited
 
+def getBagsRequired(adjacencies, start):
+    bagCountCache = {}
+    def _getBagsRequired(cur):
+        if cur in bagCountCache:
+            return bagCountCache[cur]
+        bagCount = 1
+        for color, count in adjacencies[cur]:
+            bagCount += int(count) * _getBagsRequired(color)
+        bagCountCache[cur] = bagCount
+        return bagCount
+    return _getBagsRequired(start)
+
 def partOne(adjacencies):
     return len(traverseAdjacencies(adjacencies, 'shiny gold')) - 1
+
+def partTwo(adjacencies):
+    return getBagsRequired(adjacencies, 'shiny gold') - 1
 
 def main(argv):
     Util.setDay(7)
     parser = Util.getArgumentParser()
     args = parser.parse_args(argv)
     bagRules = getRules(args.filename)
-    colorAdjacencies = getColorAdjacencies(bagRules)
-    print(colorAdjacencies)
-    Util.printSolutions([partOne(colorAdjacencies)])
+    forwardAdjacencies, backwardAdjacencies = getAdjacencies(bagRules)
+    Util.printSolutions([
+        partOne(backwardAdjacencies),
+        partTwo(forwardAdjacencies)])
 
 if __name__ == '__main__':
     sys.exit(main(sys.argv[1:]))
