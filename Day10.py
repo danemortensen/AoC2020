5d34a4802f3450e6e5004f7476e0fbdb45f89d71
Dane Mortensen <mortensendane@gmail.com>
2020-12-13 23:04:38 -0800

    Day 10 solution part 2

--- Day10.py
+++ Day10.py
@@ -18,15 +18,41 @@ def deltaFreqs(adapters):
 
 def partOne(adapters):
     freqs = deltaFreqs(adapters)
-    return deltaFreqs[1] * deltaFreqs[3]
+    return freqs[1] * freqs[3]
+
+def getJoltageTree(joltages):
+    joltageTree = {}
+    joltages.sort()
+    joltages.insert(0, 0)
+    joltages.append(joltages[-1] + 3)
+    for i, joltage in enumerate(joltages):
+        joltageTree[joltage] = []
+        for j in joltages[i + 1:i + 4]:
+            if j > joltage + 3:
+                break
+            joltageTree[joltage].append(j)
+    return joltageTree
+
+def countValidAdapterChains(joltageTree):
+    counts = {}
+    def _count(root):
+        nonlocal joltageTree, counts
+        if root not in counts:
+            if not joltageTree[root]:
+                return 1
+            counts[root] = sum(_count(j) for j in joltageTree[root])
+        return counts[root]
+    return _count(0)
+
+def partTwo(adapters):
+    return countValidAdapterChains(getJoltageTree(adapters))
 
 def main(argv):
     Util.setDay(10)
     parser = Util.getArgumentParser()
     args = parser.parse_args(argv)
     adapters = Util.parseFile(args.filename, Util.intFileHandler)
-    freqs = deltaFreqs(adapters)
-    Util.printSolutions([partOne(adapters)])
+    Util.printSolutions([partOne(adapters), partTwo(adapters)])
 
 if __name__ == '__main__':
     sys.exit(main(sys.argv[1:]))
